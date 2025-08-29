def8390b5f69bc2e0ede048eb6188edf329f1956
0luwafemi <fgbadamosi@swifta.com>
2025-08-29 20:57:50 +0100

    forget_password add in loginScreen

--- lib/main.dart
+++ lib/main.dart
@@ -81,13 +81,42 @@ class HomeHuntRoot extends StatelessWidget {
       themeMode: themeProvider.themeMode,
       initialRoute: RouteNames.home,
       onGenerateRoute: (settings) {
-        final page = AppRoutes.routes[settings.name];
-        if (page == null) {
-          debugPrint('Unknown route: ${settings.name}');
+        final args = settings.arguments;
+
+        switch (settings.name) {
+          case RouteNames.home:
+            return MaterialPageRoute(builder: (_) => const HomeScreen());
+          case RouteNames.profile:
+            return MaterialPageRoute(builder: (_) => const ProfileScreen());
+          case RouteNames.propertySearch:
+            return MaterialPageRoute(builder: (_) => PropertySearchScreen());
+          case RouteNames.transaction:
+            return MaterialPageRoute(builder: (_) => const TransactionScreen());
+          case RouteNames.dashboard:
+            final token = args is String ? args : '';
+            return MaterialPageRoute(builder: (_) => DashboardScreen(restToken: token));
+          case RouteNames.login:
+            return MaterialPageRoute(builder: (_) => const LoginScreen());
+          case RouteNames.register:
+            return MaterialPageRoute(builder: (_) => const RegistrationScreen());
+          case RouteNames.changePassword:
+            return MaterialPageRoute(builder: (_) => const ChangePasswordScreen());
+          case RouteNames.forgetPassword:
+            return MaterialPageRoute(builder: (_) => const ForgotPasswordScreen());
+          case RouteNames.resetToken:
+            return MaterialPageRoute(builder: (_) => const ResendTokenScreen());
+          case RouteNames.feedback:
+            return MaterialPageRoute(builder: (_) => const FeedbackScreen());
+          case RouteNames.main:
+            return MaterialPageRoute(builder: (_) => const MainScreen());
+          case RouteNames.counter:
+            return MaterialPageRoute(builder: (_) => const CounterScreen());
+          case RouteNames.properties:
+            return MaterialPageRoute(builder: (_) => const PropertyScreen());
+          default:
+            debugPrint('Unknown route: ${settings.name}');
+            return MaterialPageRoute(builder: (_) => const RegistrationScreen());
         }
-        return MaterialPageRoute(
-          builder: (_) => page ?? const RegistrationScreen(),
-        );
       },
     );
   }
@@ -110,25 +139,137 @@ class RouteNames {
   static const properties = '/properties';
 }
 
-class AppRoutes {
-  static final Map<String, Widget> routes = {
-    RouteNames.home: const HomeScreen(),
-    RouteNames.profile: const ProfileScreen(),
-    RouteNames.propertySearch: PropertySearchScreen(),
-    RouteNames.transaction: const TransactionScreen(),
-    RouteNames.dashboard: const DashboardScreen(restToken: '',),
-    RouteNames.login: const LoginScreen(),
-    RouteNames.register: const RegistrationScreen(),
-    RouteNames.changePassword: const ChangePasswordScreen(),
-    RouteNames.forgetPassword: const ForgotPasswordScreen(),
-    RouteNames.resetToken: const ResendTokenScreen(),
-    RouteNames.feedback: const FeedbackScreen(),
-    RouteNames.main: const MainScreen(),
-    RouteNames.counter: const CounterScreen(),
-    RouteNames.properties: const PropertyScreen(),
-  };
-}
-
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+//
+// // Providers
+// import 'package:homehunt/providers/theme_provider.dart';
+// import 'package:homehunt/providers/auth_provider.dart';
+// import 'package:homehunt/providers/property_provider.dart';
+// import 'package:homehunt/providers/property_search_provider.dart';
+// import 'package:homehunt/providers/transection_provider.dart';
+// import 'package:homehunt/providers/feedback_provider.dart';
+//
+// // Services
+// import 'package:homehunt/services/property_service.dart';
+// import 'package:homehunt/services/transection_service.dart';
+// import 'package:homehunt/services/feedback_service.dart';
+//
+// // Screens
+// import 'package:homehunt/views/home_screen.dart';
+// import 'package:homehunt/views/profile_screen.dart';
+// import 'package:homehunt/views/property_search_screen.dart';
+// import 'package:homehunt/views/transection_screen.dart';
+// import 'package:homehunt/views/dashboard_screen.dart';
+// import 'package:homehunt/views/login_screen.dart';
+// import 'package:homehunt/views/registration_screen.dart';
+// import 'package:homehunt/views/change_password_screen.dart';
+// import 'package:homehunt/views/forget_password_screen.dart';
+// import 'package:homehunt/views/reset_token_screen.dart';
+// import 'package:homehunt/views/feedback_screen.dart';
+// import 'package:homehunt/views/main_screen.dart';
+// import 'package:homehunt/views/counter_screen.dart';
+// import 'package:homehunt/views/property_screen.dart';
+//
+// void main() {
+//   runApp(const HomeHuntApp());
+// }
+//
+// class HomeHuntApp extends StatelessWidget {
+//   const HomeHuntApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (_) => ThemeProvider()),
+//         ChangeNotifierProvider(create: (_) => AuthProvider()),
+//         ChangeNotifierProvider(create: (_) => PropertySearchProvider()),
+//         Provider(create: (_) => PropertyService()),
+//         ChangeNotifierProvider(
+//           create: (context) => PropertyProvider(context.read<PropertyService>()),
+//         ),
+//         Provider(create: (_) => TransactionService()),
+//         ChangeNotifierProvider(
+//           create: (context) => TransactionProvider(context.read<TransactionService>()),
+//         ),
+//         Provider(create: (_) => FeedbackService()),
+//         ChangeNotifierProvider(
+//           create: (context) => FeedbackProvider(context.read<FeedbackService>()),
+//         ),
+//       ],
+//       child: const HomeHuntRoot(),
+//     );
+//   }
+// }
+//
+// class HomeHuntRoot extends StatelessWidget {
+//   const HomeHuntRoot({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final themeProvider = Provider.of<ThemeProvider>(context);
+//
+//     return MaterialApp(
+//       title: 'Home Hunt',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         brightness: Brightness.light,
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       darkTheme: ThemeData.dark(),
+//       themeMode: themeProvider.themeMode,
+//       initialRoute: RouteNames.home,
+//       onGenerateRoute: (settings) {
+//         final page = AppRoutes.routes[settings.name];
+//         if (page == null) {
+//           debugPrint('Unknown route: ${settings.name}');
+//         }
+//         return MaterialPageRoute(
+//           builder: (_) => page ?? const RegistrationScreen(),
+//         );
+//       },
+//     );
+//   }
+// }
+//
+// class RouteNames {
+//   static const home = '/home';
+//   static const profile = '/profile';
+//   static const propertySearch = '/propertySearch';
+//   static const transaction = '/transaction';
+//   static const dashboard = '/dashboard';
+//   static const login = '/login';
+//   static const register = '/register';
+//   static const changePassword = '/changePassword';
+//   static const forgetPassword = '/forgetPassword';
+//   static const resetToken = '/resetToken';
+//   static const feedback = '/feedback';
+//   static const main = '/main';
+//   static const counter = '/counter';
+//   static const properties = '/properties';
+// }
+//
+// class AppRoutes {
+//   static final Map<String, Widget> routes = {
+//     RouteNames.home: const HomeScreen(),
+//     RouteNames.profile: const ProfileScreen(),
+//     RouteNames.propertySearch: PropertySearchScreen(),
+//     RouteNames.transaction: const TransactionScreen(),
+//     RouteNames.dashboard: const DashboardScreen(restToken: '',),
+//     RouteNames.login: const LoginScreen(),
+//     RouteNames.register: const RegistrationScreen(),
+//     RouteNames.changePassword: const ChangePasswordScreen(),
+//     RouteNames.forgetPassword: const ForgotPasswordScreen(),
+//     RouteNames.resetToken: const ResendTokenScreen(),
+//     RouteNames.feedback: const FeedbackScreen(),
+//     RouteNames.main: const MainScreen(),
+//     RouteNames.counter: const CounterScreen(),
+//     RouteNames.properties: const PropertyScreen(),
+//   };
+// }
+//
 
 // import 'package:flutter/material.dart';
 // import 'package:homehunt/providers/property_search_provider.dart';
--- lib/views/forget_password_screen.dart
+++ lib/views/forget_password_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -18,6 +17,17 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   bool _isLoading = false;
   String? _errorMessage;
   bool _obscureNew = true;
+  String? _resetToken;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Retrieve the token passed via Navigator
+    final args = ModalRoute.of(context)?.settings.arguments;
+    if (args is String) {
+      _resetToken = args;
+    }
+  }
 
   void _setLoading(bool value) {
     setState(() => _isLoading = value);
@@ -30,6 +40,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   Future<bool> forgotPassword({
     required String email,
     required String newPassword,
+    required String resetToken,
   }) async {
     _setLoading(true);
     _setError(null);
@@ -38,6 +49,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     final body = {
       'email': email,
       'newPassword': newPassword,
+      'resetToken': resetToken, // Include token in request
     };
 
     try {
@@ -68,7 +80,16 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     final email = _emailController.text.trim();
     final newPassword = _newPasswordController.text;
 
-    final success = await forgotPassword(email: email, newPassword: newPassword);
+    if (_resetToken == null) {
+      _setError('Missing reset token');
+      return;
+    }
+
+    final success = await forgotPassword(
+      email: email,
+      newPassword: newPassword,
+      resetToken: _resetToken!,
+    );
 
     if (!mounted) return;
 
@@ -154,3 +175,161 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     );
   }
 }
+
+
+// import 'dart:convert';
+//
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+//
+// class ForgotPasswordScreen extends StatefulWidget {
+//   const ForgotPasswordScreen({super.key});
+//
+//   @override
+//   State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+// }
+//
+// class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+//   final _formKey = GlobalKey<FormState>();
+//   final _emailController = TextEditingController();
+//   final _newPasswordController = TextEditingController();
+//
+//   bool _isLoading = false;
+//   String? _errorMessage;
+//   bool _obscureNew = true;
+//
+//   void _setLoading(bool value) {
+//     setState(() => _isLoading = value);
+//   }
+//
+//   void _setError(String? message) {
+//     setState(() => _errorMessage = message);
+//   }
+//
+//   Future<bool> forgotPassword({
+//     required String email,
+//     required String newPassword,
+//   }) async {
+//     _setLoading(true);
+//     _setError(null);
+//
+//     final url = Uri.parse('https://your-api.com/forgot-password');
+//     final body = {
+//       'email': email,
+//       'newPassword': newPassword,
+//     };
+//
+//     try {
+//       final response = await http.post(
+//         url,
+//         headers: {'Content-Type': 'application/json'},
+//         body: jsonEncode(body),
+//       );
+//
+//       _setLoading(false);
+//
+//       if (response.statusCode == 200) {
+//         return true;
+//       } else {
+//         _setError('Failed to reset password: ${response.body}');
+//         return false;
+//       }
+//     } catch (e) {
+//       _setLoading(false);
+//       _setError('Error: $e');
+//       return false;
+//     }
+//   }
+//
+//   Future<void> _handleSubmit() async {
+//     if (!_formKey.currentState!.validate()) return;
+//
+//     final email = _emailController.text.trim();
+//     final newPassword = _newPasswordController.text;
+//
+//     final success = await forgotPassword(email: email, newPassword: newPassword);
+//
+//     if (!mounted) return;
+//
+//     if (success) {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Password reset successful')),
+//       );
+//       Navigator.pop(context);
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text(_errorMessage ?? 'Unknown error')),
+//       );
+//     }
+//   }
+//
+//   @override
+//   void dispose() {
+//     _emailController.dispose();
+//     _newPasswordController.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Forgot Password')),
+//       body: Padding(
+//         padding: const EdgeInsets.all(24),
+//         child: Form(
+//           key: _formKey,
+//           child: ListView(
+//             children: [
+//               if (_errorMessage != null)
+//                 Padding(
+//                   padding: const EdgeInsets.only(bottom: 12),
+//                   child: Text(
+//                     _errorMessage!,
+//                     style: const TextStyle(color: Colors.red),
+//                   ),
+//                 ),
+//               TextFormField(
+//                 controller: _emailController,
+//                 decoration: const InputDecoration(
+//                   labelText: 'Email',
+//                   prefixIcon: Icon(Icons.email),
+//                   border: OutlineInputBorder(),
+//                 ),
+//                 keyboardType: TextInputType.emailAddress,
+//                 validator: (value) =>
+//                 value != null && value.contains('@') ? null : 'Enter a valid email',
+//               ),
+//               const SizedBox(height: 16),
+//               TextFormField(
+//                 controller: _newPasswordController,
+//                 obscureText: _obscureNew,
+//                 decoration: InputDecoration(
+//                   labelText: 'New Password',
+//                   prefixIcon: const Icon(Icons.lock_outline),
+//                   suffixIcon: IconButton(
+//                     icon: Icon(_obscureNew ? Icons.visibility : Icons.visibility_off),
+//                     onPressed: () => setState(() => _obscureNew = !_obscureNew),
+//                   ),
+//                   border: const OutlineInputBorder(),
+//                 ),
+//                 validator: (value) =>
+//                 value != null && value.length >= 6 ? null : 'Password must be at least 6 characters',
+//               ),
+//               const SizedBox(height: 24),
+//               SizedBox(
+//                 width: double.infinity,
+//                 child: _isLoading
+//                     ? const Center(child: CircularProgressIndicator())
+//                     : ElevatedButton.icon(
+//                   icon: const Icon(Icons.refresh),
+//                   label: const Text('Reset Password'),
+//                   onPressed: _handleSubmit,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
